838146fc14ac60039fc9a92c23c3c0ed1446bd94
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-10-22 18:17:47 -0300

    CategoriasController/ Endpoints GET POST PUT DELETE

---
+++ CatalogoApi/Controllers/CategoriasController.cs
@@ -0,0 +1,78 @@
+﻿using CatalogoApi.Context;
+using CatalogoApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoApi.Controllers
+{
+    [Route("[Controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            return _context.Categorias.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _context.Categorias.AsNoTracking().FirstOrDefault(c => c.CategoriaId == id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return categoria;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Categoria categoria)
+        {
+            //Validação do modelo é feita automaticamente a partir do ASP.NET 2.0 com a anotação ApiController
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+
+            //Retorna um header location
+            return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest();
+            }
+            //Alterar o estado da entidade para Modified, fazer as alterações e persistir no banco
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Categoria> Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+            return categoria;
+        }
+    }
+}
