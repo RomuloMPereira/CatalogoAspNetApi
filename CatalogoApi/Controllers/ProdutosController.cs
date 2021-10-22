f0a71e695862e720353f0ad6689cbb9d1b5498ef
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-10-22 14:54:27 -0300

    ProdutosController/ Endpoints GET

---
+++ CatalogoApi/Controllers/ProdutosController.cs
@@ -0,0 +1,39 @@
+﻿using CatalogoApi.Context;
+using CatalogoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoApi.Controllers
+{
+    [Route("[Controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            return _context.Produtos.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.AsNoTracking().FirstOrDefault(p => p.ProdutoId == id);
+            if(produto == null)
+            {
+                return NotFound();
+            }
+            return produto;
+        }
+    }
+}
